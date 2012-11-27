765227ee6f14ef5cc462f4e16360eb5bd5619435
Charles Cary <charles@ip-10-38-134-116.ec2.internal>
2012-11-27 20:05:15 +0000

    create-image now converts whole output directory to specified file format

--- create-image.py
+++ create-image.py
@@ -1,10 +1,22 @@
+import sys
+import os
 import Image
 import numpy
 import scipy
 
-filename = "output/1.bin"
+if (len(sys.argv) < 3):
+  print "usage: python create-image.py <path to bin files> <output image file type>"
+  sys.exit(0)
 
-datalinear = numpy.fromfile(filename, dtype=numpy.uint8).reshape(32,32,3, order="F").copy()
-img = scipy.misc.toimage(datalinear)
-img.save("output/1.bmp")
+path = sys.argv[1]
+img_output_type = sys.argv[2]
+
+files = os.listdir(path)
+
+for f_name in files:
+  pieces = f_name.split(".")
+  if (len(pieces) > 1 and pieces[1] == "bin"):
+    img_data_raw = numpy.fromfile(path+f_name, dtype=numpy.uint8).reshape(32,32,3, order="F").copy()
+    img = scipy.misc.toimage(img_data_raw)
+    img.save(path+pieces[0]+"."+img_output_type)
 
